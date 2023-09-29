1ee054af075e804f03278b19b47ffaef87582133
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-09-29 14:57:48 +0600

    ready firebase

--- src/__steps.js
+++ src/__steps.js
@@ -4,5 +4,6 @@
 3. register app (create config )
 4. install fire base (npm install firebase )
 5. add config file to project 
+6. Danger: do not publish firebase config to git hub by pushing it 
 
 */

b202463e5351b8faf2cd3669de48a0ea01cbb1aa
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-09-29 12:53:55 +0600

    firebase initialization done

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAz4jV9nVw72kgO0K7RcXRw2YjDwZu4n8k",
+  authDomain: "fir-authorization-741a4.firebaseapp.com",
+  projectId: "fir-authorization-741a4",
+  storageBucket: "fir-authorization-741a4.appspot.com",
+  messagingSenderId: "785739623171",
+  appId: "1:785739623171:web:a2e16b23f16cc7cd03d736"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/__steps.js
@@ -0,0 +1,8 @@
+/* 
+1.console.firebase.google.com 
+2. Create a project (skip google analytics )
+3. register app (create config )
+4. install fire base (npm install firebase )
+5. add config file to project 
+
+*/
